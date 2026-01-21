4967caed14f0ef78bcbac3e4f0440c084c0aef41
society118 <glebgrigorenko35@gmail.com>
2026-01-21 10:16:33 -0600

    SQLAlchemy + FASTAPI

--- main1.py
+++ main1.py
@@ -1,23 +1,27 @@
 from typing import Annotated
+import uvicorn
 from fastapi import Depends, FastAPI
 from pydantic import BaseModel
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-import uvicorn
 
 app = FastAPI()
 engine = create_async_engine("sqlite+aiosqlite:///mydb.db", echo=True)
 new_async_session = async_sessionmaker(engine, expire_on_commit=False)
 
+
 async def get_session():
     async with new_async_session() as session:
         yield session
 
+
 SessionDep = Annotated[AsyncSession, Depends(get_session)]
+
 
 class Base(DeclarativeBase):
     pass
+
 
 class BookModel(Base):
     __tablename__ = "books"
@@ -26,20 +30,24 @@ class BookModel(Base):
     title: Mapped[str]
     author: Mapped[str]
 
+
 class BookSchema(BaseModel):
     title: str
     author: str
+
 
 class BookGetSchema(BaseModel):
     id: int
     title: str
     author: str
 
+
 @app.post("/setup")
 async def setup_database():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
+
 
 @app.post("/books")
 async def add_book(book: BookSchema, session: SessionDep) -> BookSchema:
@@ -51,6 +59,7 @@ async def add_book(book: BookSchema, session: SessionDep) -> BookSchema:
     await session.commit()
     return book
 
+
 @app.get("/books")
 async def get_books(session: SessionDep) -> list[BookGetSchema]:
     query = select(BookModel)
@@ -59,5 +68,6 @@ async def get_books(session: SessionDep) -> list[BookGetSchema]:
     print(f"{books=}")
     return books
 
-if __name__ =='__main__':
-    uvicorn.run("schemas:app",reload=True,port=5051)
+
+if __name__ == '__main__':
+    uvicorn.run("main1:app", reload=True, port=5052)
